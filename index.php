67416edea6380aed1f91a5c89b2af35bb85bf280
unknown <kieran_may@outlook.comMinecraft2005''git config --global user.email kieran_may@outlook.com>
2018-07-24 20:11:19 +0100

    edited file *Head section* (20:10 24/07/2018) index.php

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
     </header>
 
     <body>
-        <p><marquee>km</marquee>
-        
+        <p><marquee><h1>Trucks For Sale Us</h1></marquee>
+            This is Km-McHome/Trucks Were we sale trucks & SUV at cheap prices<br>
+            
         </p>
     </body>
 </html>
