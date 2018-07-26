7fb3c2a29fef65265dfac6f670074f0413459ef9
unknown <kieran_may@outlook.comMinecraft2005''git config --global user.email kieran_may@outlook.com>
2018-07-26 14:08:21 +0100

    styling started + header edited $ *Hot Deal* 14:08 26/07/2018

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <style>
     body{
-    font-family: 'Merienda', cursive;
+        font-family: 'Merienda', cursive;
+        cursor: pointer;
+    }
+    .a{
+        height: 80px;
+        width: 100%;
+        color: darkred;
+        background: #000;
+        font-family: 'Merienda', cursive;
     }
 </style>
 <html>
@@ -10,9 +18,17 @@
     </header>
 
     <body>
-        <p><marquee><h1>Trucks For Sale Us</h1></marquee>
-            This is Km-McHome/Trucks Were we sale trucks & SUV at cheap prices. Located in the US & UK<br>
+        <p><marquee><h1>Trucks For Sale US & UK</h1></marquee><hr>
+        <h3>This is Km-McHome/Trucks Were we sale trucks & SUV at cheap prices. Located in the US & UK.</h3>
+        <hr>
             
+            
+        </p>
+    <br>
+    <br>
+        <p>
+            Recomended page for Deals as of 26/07/2018
+            <a href="Deals.php"><button class="a"><marquee><h1>Hot Deals</h1></marquee></button></a>
         </p>
     </body>
 </html>
