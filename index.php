b04219004ab6a27a696bb3f80f46371c1b31f217
unknown <kieran_may@outlook.comMinecraft2005''git config --global user.email kieran_may@outlook.com>
2018-07-28 10:44:04 +0100

    .

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <style>
     body{
+        background-image: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)),url(cars/bg2.jpg);
         font-family: 'Merienda', cursive;
         cursor: cell;
     }
@@ -9,6 +10,19 @@
         color: darkred;
         background: #000;
         font-family: 'Merienda', cursive;
+    }
+    .c {
+        background-color: #4CAF50; /* Green */
+        border: none;
+        color: white;
+        padding: 15px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 14px 12px;
+        cursor: pointer;
+        border-radius: 50%;
     }
     .b{
         float: right;
@@ -33,20 +47,36 @@
     <br>
     <br>
         <p>
-            Recomended page for Deals as of 26/07/2018
+    <h4>Recomended page for Deals as of 26/07/2018</h4>
             <a href="Deals.php"><button class="a"><marquee><h1>Hot Deals</h1></marquee></button></a>
-            <table>
+            <center><table>
                 <tr>
                     <td>
-                        <img src="car.jpg" width="500px" height="300px">
+                        <img src="cars/car.jpg" width="500px" height="300px">
+                    </td>
+                    <td>
+                        <img src="cars/Pcar.jpg" width="500px" height="300px">
+                    </td>
+                    <td>
+                        <img src="cars/2car.jpg" width="500px" height="300px">
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        Land Rover Discovery Price tag: $4640.
-                    </td> 
+                        Land Rover Discovery Price tag: $1640. Second Hand &<br> Miles 10930.
+                    </td>
+                    <td>
+                        Decomitioned police car Price tag: $64650.<br>
+                        Miles 5320.
+                    </td>
+                    <td>
+                        Nissan Navara TEKNA 4x4 Pickup Price tag: $15490.<br>
+                        Second Hand & Miles 8340
                 </tr>
-    </table>
+            </table></center>
+    <hr>
+    <center>Click Here to Veiw more<a href="Deals.php"><button class="c"><b>↓</b></button></a></center>
+    <hr><hr>
         </p>
     </body>
 </html>
